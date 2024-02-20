d805c8e11e529ec52ccf8dad6080fc5a8966ae92
FilMarini <marinifil@gmail.com>
2024-02-20 11:01:16 +0100

    changed setup for ubuntu 20.04

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ set -o xtrace
 
 rm -rf bsc-*
 # wget https://github.com/B-Lang-org/bsc/releases/download/2022.01/bsc-2022.01-ubuntu-20.04.tar.gz
-wget https://github.com/B-Lang-org/bsc/releases/download/2023.01/bsc-2023.01-ubuntu-22.04.tar.gz
+wget https://github.com/B-Lang-org/bsc/releases/download/2023.01/bsc-2023.01-ubuntu-20.04.tar.gz
 tar zxf bsc-*
 
 BSC_FILE_NAME=`ls bsc-*.tar.gz`
